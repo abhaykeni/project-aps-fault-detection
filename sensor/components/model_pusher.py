9566562232bed6e696a286ef0bf505485002a98d
abhaykeni <abhaykeni@gmail.com>
2022-12-29 04:22:28 +0530

    Model Pusher Completed

--- sensor/components/model_pusher.py
+++ sensor/components/model_pusher.py
@@ -16,29 +16,38 @@ class ModelPusher:
             self.model_pusher_config = model_pusher_config
             self.data_transformation_artifact = data_transformation_artifact
             self.model_trainer_artifact = model_trainer_artifact
-            self.ModelResolver = ModelResolver(model_registry=self.model_pusher_config.saved_model_dir)
+            self.model_resolver = ModelResolver(model_registry=self.model_pusher_config.saved_model_dir)
         except Exception as e:
             raise SensorException
     
     def initiate_model_pusher(self)->ModelPusherArtifact:
         try:
+            #load object
             logging.info(f"Loading transformer model and target encoder")
             transformer = load_object(file_path=self.data_transformation_artifact.transform_object_path)
             model = load_object(file_path=self.model_trainer_artifact.model_path)
             target_encoder = load_object(file_path=self.data_transformation_artifact.target_encoder_path)
 
+            #model pusher dir
+            logging.info(f"Saving model into model pusher directory")
             save_object(file_path=self.model_pusher_config.pusher_transformer_path, obj=transformer)
             save_object(file_path=self.model_pusher_config.pusher_model_path, obj=model)
             save_object(file_path=self.model_pusher_config.pusher_target_encoder_path, obj=target_encoder)
 
+            logging.info(f"Saving model in saved model dir")
+            transformer_path=self.model_resolver.get_latest_save_transformer_path()
+            model_path=self.model_resolver.get_latest_save_model_path()
+            target_encoder_path=self.model_resolver.get_latest_save_target_encoder_path()
 
-            save_object(file_path=self.model_resolver.get_latest_save_transformer_path(), obj=transformer)
-            save_object(file_path=self.model_resolver.get_latest_save_model_path(), obj=model)
-            save_object(file_path=self.model_resolver.get_latest_save_target_encoder_path(), obj=target_encoder)
+            #saved model dir
+            logging.info(f"Saving model in saved model dir")
+            save_object(file_path=transformer_path, obj=transformer)
+            save_object(file_path=model_path, obj=model)
+            save_object(file_path=target_encoder_path, obj=target_encoder)
 
             model_pusher_artifact = ModelPusherArtifact(pusher_model_dir=self.model_pusher_config.pusher_model_dir, 
             saved_model_dir=self.model_pusher_config.saved_model_dir)
             logging.info(f"Model pusher artifact: {model_pusher_artifact}")
             return model_pusher_artifact
-        except:
-            raise SensorException
+        except Exception as e:
+            raise SensorException(e,sys)
