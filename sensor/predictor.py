d648cff2702cea8a173f139da0957a774b65504c
abhaykeni <abhaykeni@gmail.com>
2022-12-29 11:57:37 +0530

    Training Pipeline and Batch Predication Completed

--- sensor/predictor.py
+++ sensor/predictor.py
@@ -18,7 +18,7 @@ class ModelResolver:
             self.target_encoder_dir_name = target_encoder_dir_name
             self.model_dir_name = model_dir_name
         except Exception as e:
-            raise SensorException
+            raise SensorException(e,sys)
 
     
 
@@ -27,11 +27,11 @@ class ModelResolver:
             dir_names = os.listdir(self.model_registry)
             if len(dir_names)==0:
                 return None
-            dir_names = list(map(dir_names))
+            dir_names = list(map(int,dir_names))
             latest_folder_names = max(dir_names)
             return os.path.join(self.model_registry,f"{latest_folder_names}")
         except Exception as e:
-            raise SensorException
+            raise SensorException(e,sys)
     
     def get_latest_model_path(self,):
         try:
@@ -40,7 +40,7 @@ class ModelResolver:
                 raise Exception(f"Model is not available")
             return os.path.join(latest_dir,self.model_dir_name,MODEL_FILE_NAME)
         except Exception as e:
-            raise SensorException
+            raise SensorException(e,sys)
 
     def get_latest_transformer_path(self,):
         try:
@@ -49,7 +49,7 @@ class ModelResolver:
                 raise Exception(f"Transformer is not available")
             return os.path.join(latest_dir,self.transoformer_dir_name,TRANSFORMER_OBJECT_FILE_NAME)
         except Exception as e:
-            raise SensorException
+            raise SensorException(e,sys)
 
     def get_latest_target_encoder_path(self,):
         try:
@@ -58,7 +58,7 @@ class ModelResolver:
                 raise Exception(f"Target Encoder is not available")
             return os.path.join(latest_dir,self.target_encoder_dir_name,TARGET_ENCODER_OBJECT_FILE_NAME)
         except Exception as e:
-            raise SensorException
+            raise SensorException(e,sys)
 
     def get_latest_save_dir_path(self)->str:
         try:
@@ -68,25 +68,25 @@ class ModelResolver:
             latest_dir_num = int(os.path.basename(self.get_latest_dir_path()))
             return os.path.join(self.model_registry,f"{latest_dir_num+1}")           
         except Exception as e:
-            raise SensorException
+            raise SensorException(e,sys)
 
     def get_latest_save_model_path(self,):
         try:
             latest_dir = self.get_latest_save_dir_path()
             return os.path.join(latest_dir,self.model_dir_name,MODEL_FILE_NAME)
         except Exception as e:
-            raise SensorException
+            raise SensorException(e,sys)
 
     def get_latest_save_transformer_path(self,):
         try:
             latest_dir = self.get_latest_save_dir_path()
             return os.path.join(latest_dir,self.transoformer_dir_name,TRANSFORMER_OBJECT_FILE_NAME)
         except Exception as e:
-            raise SensorException
+            raise SensorException(e,sys)
 
     def get_latest_save_target_encoder_path(self,):
         try:
             latest_dir = self.get_latest_save_dir_path()
             return os.path.join(latest_dir,self.target_encoder_dir_name,TARGET_ENCODER_OBJECT_FILE_NAME)
         except Exception as e:
-            raise SensorException
+            raise SensorException(e,sys)
